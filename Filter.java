e68449041b93b65c294bd50f2ffa0d57dea64ed9
Pradhip <96421552+PradhipJ@users.noreply.github.com>
2024-01-09 16:19:11 +0530

    Update Filter.java

--- Filter.java
+++ Filter.java
@@ -5,20 +5,20 @@ interface Criteria {
 }
 
 public class Filter {
-    static ArrayList<String> filterArray(String[] words){
-        Criteria startsWithB = (word) -> {
-          if(word.charAt(0) == 'b') return true;
-          else return false;
-        };
+    static ArrayList<String> filterArray(String[] words, Criteria criteria){
         ArrayList<String> result = new ArrayList<>();
-        for(int i = 0; i < arr.length; i++){
-          if(startsWithB.apply(arr[i])) result.add(arr[i]);
+        for(int i = 0; i < words.length; i++){
+          if(criteria.apply(words[i])) result.add(words[i]);
         }
         return result;
     }
     public static void main(String args[]) {
       String[] arr = new String[]{"abc", "abcd", "bcd"};
-      ArrayList<String> res = filterArray(arr);
+       Criteria startsWithB = (word) -> {
+          if(word.charAt(0) == 'b') return true;
+          else return false;
+        };
+      ArrayList<String> res = filterArray(arr, startsWithB);
       System.out.println(res.toString());
     }
 }
