81b89ebd040d33ff8ad613f41db76c510d68e742
Pradhip <96421552+PradhipJ@users.noreply.github.com>
2024-01-09 15:39:36 +0530

    Update Filter.java

--- Filter.java
+++ Filter.java
@@ -1,19 +1,19 @@
 import java.util.*;
 
 interface Criteria {
-    boolean startsWithA(String word);
+    boolean apply(String word);
 }
 
 public class Filter {
     public static void main(String args[]) {
       String[] arr = new String[]{"abc", "abcd", "bcd"};
-      Criteria criteria = (word) -> {
-          if(word.charAt(0) == 'a') return true;
+      Criteria startsWithB = (word) -> {
+          if(word.charAt(0) == 'b') return true;
           else return false;
       };
       ArrayList<String> result = new ArrayList<>();
       for(int i = 0; i < arr.length; i++){
-          if(criteria.startsWithA(arr[i])) result.add(arr[i]);
+          if(startsWithB.apply(arr[i])) result.add(arr[i]);
       }
       System.out.println(result.toString());
     }
