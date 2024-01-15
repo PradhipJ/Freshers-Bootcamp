b4531ef8b0fd18d2c4a9df93262ac3764c1a7e20
Pradhip <96421552+PradhipJ@users.noreply.github.com>
2024-01-15 11:11:19 +0530

    Add files via upload

---
+++ ObserverPattern.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+interface IObserver {
+    public void update(String message);
+}
+
+class Dashboard implements IObserver{
+    String message;
+    public void update(String message){
+        this.message = message;
+        System.out.println("Thread state changed to " + message);
+    }
+}
+
+class Thread {
+    String id;
+    String state;
+    String priority;
+    String culture;
+    ArrayList<IObserver> observers;
+    
+    public Thread(String id, String priority, String culture){
+        this.id = id;
+        this.state = "created";
+        this.priority = priority;
+        this.culture = culture;
+        observers = new ArrayList<>();
+    }
+    
+    private void notifyObservers(){
+        for(IObserver observer: observers){
+            observer.update(this.state);
+        }
+    }
+    
+    public void start(){
+        this.state = "running";
+        notifyObservers();
+    }
+    
+    public void abort(){
+        this.state = "aborted";
+        notifyObservers();
+    }
+    
+    public void sleep(){
+        this.state = "sleeping";
+        notifyObservers();
+    }
+    
+    public void suspend(){
+        this.state = "suspended";
+        notifyObservers();
+    }
+    
+    public void subscribe(IObserver observer){
+        observers.add(observer);
+    }
+    
+    public void unsubscribe(IObserver observer){
+        observers.remove(observer);
+    }
+    
+}
+
+public class ObserverPattern {
+    public static void main(String args[]) {
+      Thread t = new Thread("1A", "low", "en-US");
+      IObserver dashboard = new Dashboard();
+      t.subscribe(dashboard);
+      t.start();
+      t.abort();
+      t.sleep();
+      t.suspend();
+      t.unsubscribe(dashboard);
+    }
+}
