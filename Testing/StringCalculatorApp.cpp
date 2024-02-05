625933318020e70c2169c665e3b97f0b83668975
Pradhip <96421552+PradhipJ@users.noreply.github.com>
2024-02-05 14:22:23 +0530

    Update StringCalculatorApp.cpp

--- Testing/StringCalculatorApp.cpp
+++ Testing/StringCalculatorApp.cpp
@@ -12,18 +12,5 @@ int main()
 {
     testing::InitGoogleTest();
     return RUN_ALL_TESTS();
-    /* try {
-        cout << "Test Case 1: " << StringCalculator::addNumbers("") << endl; 
-        cout << "Test Case 2: " << StringCalculator::addNumbers("0") << endl;
-        cout << "Test Case 3: " << StringCalculator::addNumbers("1") << endl;
-        cout << "Test Case 4: " << StringCalculator::addNumbers("1,2") << endl;
-        cout << "Test Case 5: " << StringCalculator::addNumbers("1,2,3,-1,4,5") << endl;
-        cout << "Test Case 6: " << StringCalculator::addNumbers("1,1001,3,4,5") << endl;
-    }
-    catch (const invalid_argument& e) {
-        cerr << "Error: " << e.what() << endl;
-    }
-    return 0;
-    */
 }
 
